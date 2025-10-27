10523424e3ee7d7471eff1b2588efd6942fb3561
kenant68 <tekbask8@gmail.com>
2025-10-27 20:26:12 +0100

    ajout du fichier router ainsi que de l'outlet dans l'App.jsx

---
+++ src/components/routes/router.jsx
@@ -0,0 +1,35 @@
+ import {createBrowserRouter} from "react-router-dom";
+import App from "../../App.jsx"
+import Home from "../../pages/home/Home.jsx"
+import Register from "../../pages/auth/Register.jsx";
+import Login from "../../pages/auth/Login.jsx";
+
+export const router = createBrowserRouter([
+
+    {
+        path: "/",
+        element: (
+
+            <App/>
+
+        ),
+        children: [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path: "register",
+                element: (
+                    <Register/>
+                )
+            },
+            {
+                path: "login",
+                element: (
+                    <Login/>
+                )
+            },
+        ]
+    },
+])
