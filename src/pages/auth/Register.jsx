8478efcdacaad400ca66f16bcfb10a7cce61323c
kenant68 <tekbask8@gmail.com>
2025-10-27 20:47:49 +0100

    ajout de la page de register pour test du routage

---
+++ src/pages/auth/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthcardRegister from "../../components/AuthCardRegister/AuthcardRegister.jsx";
+
+const Register = () => {
+    return (
+        <div>
+            <AuthcardRegister title={"Inscription"}/>
+        </div>
+    );
+};
+
+export default Register;
