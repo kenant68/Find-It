5e2fe413202e605804dd56bb71ddc024939743c8
kenant68 <tekbask8@gmail.com>
2025-11-11 14:12:47 +0100

    Ajout des modifications stashed depuis dev

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import Navbar from "../../components/Navbar/Navbar.jsx";
+import Button from "../../../../src/components/button/Button.jsx";
+import styles from "./Home.module.css";
 
 
 const Home = () => {
     return (
         <div>
-            <Navbar />
-            <h1>Home Page</h1>
+            <div className={styles.title}>
+                <h1>FindIT</h1>
+                <p>L'application pour programmer vos scrims</p>
+            </div>
+            <div className={styles.divButtons}>
+                <Button text={"S'inscrire"}></Button>
+                <Button text={"Se connecter"}></Button>
+            </div>
         </div>
     );
 };
