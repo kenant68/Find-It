1c15971223d6acf8d8ab50e38da6b21d584ba8b1
kenant68 <tekbask8@gmail.com>
2025-11-13 20:25:35 +0100

    ajout du logo de l'appli et du retour à l 'accueil après clique

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Login.module.css'
 import AuthcardLogin from "../../components/AuthCardLogin/AuthcardLogin.jsx";
+import Logo from "../../components/icons/Logo.jsx";
+import {Link} from "react-router-dom";
 
 const Login = () => {
     return (
         <div>
+            <Link to={"/"}><Logo></Logo></Link>
             <AuthcardLogin title={"Se connecter"}/>
         </div>
     );
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import AuthcardRegister from "../../components/AuthCardRegister/AuthcardRegister.jsx";
 import styles from './Register.module.css'
+import Logo from "../../components/icons/Logo.jsx";
+import {Link} from "react-router-dom";
 
 const Register = () => {
     return (
         <div>
+            <Link to={"/"}><Logo></Logo></Link>
             <AuthcardRegister title={"Inscription"}/>
         </div>
     );
