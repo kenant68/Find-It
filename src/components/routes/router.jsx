e23ee3af58bfac7165aef92c14beea599f2f375c
kenant68 <tekbask8@gmail.com>
2025-11-15 15:22:40 +0100

    mise en commentaire des routes car components pas encore crées

--- src/components/routes/router.jsx
+++ src/components/routes/router.jsx
@@ -30,22 +30,22 @@ export const router = createBrowserRouter([
                     <Login/>
                 )
             },
-            {
-                path: "teams",
-                element: <Teams/>
-            },
-            {
-                path: "matchs",
-                element: <Matchs/>
-            },
-            {
-                path: "announces",
-                element: <Announces/>
-            },
-            {
-                path: "notifications",
-                element: <Notifications/>
-            }
+            // {
+            //     path: "teams",
+            //     element: <Teams/>
+            // },
+            // {
+            //     path: "matchs",
+            //     element: <Matchs/>
+            // },
+            // {
+            //     path: "announces",
+            //     element: <Announces/>
+            // },
+            // {
+            //     path: "notifications",
+            //     element: <Notifications/>
+            // }
         ]
     },
 ])
