ea666e0a6aab9b1b3287dc7c17242ed34b471f81
kenant68 <tekbask8@gmail.com>
2025-11-22 19:52:35 +0100

    correction du css de la page login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import styles from './Login.module.css'
+import React from "react";
+import styles from "./Login.module.css";
 import AuthcardLogin from "../../components/AuthCardLogin/AuthcardLogin.jsx";
 import Logo from "../../components/icons/Logo.jsx";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <Link to={"/"}><Logo></Logo></Link>
-            <AuthcardLogin title={"Se connecter"}/>
-        </div>
-    );
+  return (
+    <div className={styles.pageWrapper}>
+      <Link to={"/"}>
+        <Logo></Logo>
+      </Link>
+      <div className={styles.cardContainer}>
+        <AuthcardLogin title={"Se connecter"} />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
