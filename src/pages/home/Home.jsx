9116276a68740d428ef13aece833726604124cb1
kenant68 <tekbask8@gmail.com>
2025-11-22 18:54:36 +0100

    correctif du css de la page home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 import Button from "../../components/button/Button.jsx";
 import styles from "./Home.module.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from "../../components/icons/Logo.jsx";
 
-
 const Home = () => {
-    return (
-        <div>
-
-            <Logo></Logo>
-            <div className={styles.title}>
-                <h1>FindIT</h1>
-                <p>L'application pour programmer vos scrims</p>
-            </div>
-            <div className={styles.divButtons}>
-                <Link to="/register" className={styles.linkToLogin}>
-                    <Button text={"S'inscrire"}></Button>
-                </Link>
-                <Link to="/login" className={styles.linkToRegister}><Button text={"Se connecter"}></Button></Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Logo></Logo>
+      <div className={styles.title}>
+        <h1>FindIT</h1>
+        <p>L'application pour programmer vos scrims</p>
+      </div>
+      <div className={styles.divButtons}>
+        <Link to="/register" className={styles.linkToLogin}>
+          <Button text={"S'inscrire"}></Button>
+        </Link>
+        <Link to="/login" className={styles.linkToRegister}>
+          <Button text={"Se connecter"}></Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
