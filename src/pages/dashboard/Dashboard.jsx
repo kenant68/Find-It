aa9df5bff9520fc12a0a2257710479c81f7c1e09
kenant68 <tekbask8@gmail.com>
2025-11-22 19:46:50 +0100

    optimisation du css de la navbar et de la dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,22 +1,31 @@
+import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import styles from "./Dashboard.module.css";
 import Card from "../../components/Card/Card.jsx";
 import CardScrims from "../../components/CardScrims/CardScrims.jsx";
 
 const Dashboard = () => {
-    return (
-        <div className={styles.layout}>
-            <div className={styles.sidebar}>
-                <Navbar />
-            </div>
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-            <div className={styles.content}>
-                <Card>
-                    <CardScrims />
-                </Card>
-            </div>
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.layout}>
+        <div className={styles.sidebar}>
+          <Navbar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
         </div>
-    );
+
+        <div
+          className={`${styles.content} ${
+            isCollapsed ? styles.contentCollapsed : ""
+          }`}
+        >
+          <Card>
+            <CardScrims />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
