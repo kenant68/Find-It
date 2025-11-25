e105846415a391e9707d2edb555d854ac55780af
kenant68 <tekbask8@gmail.com>
2025-11-25 19:28:37 +0100

    ajout de la navbar et du background de la page

--- src/pages/notifications/Notifications.jsx
+++ src/pages/notifications/Notifications.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./Notifications.module.css";
+import Navbar from "../../components/Navbar/Navbar.jsx";
+import { useState } from "react";
 
 const Notifications = () => {
-  return <div>Notifications</div>;
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.layout}>
+        <div className={styles.sidebar}>
+          <Navbar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Notifications;
