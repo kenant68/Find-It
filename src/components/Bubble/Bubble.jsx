a45d6c66072403372bbd184ecde42d3d09323e15
kenant68 <tekbask8@gmail.com>
2025-11-29 12:43:59 +0100

    modification de la bubble et de son style

--- src/components/Bubble/Bubble.jsx
+++ src/components/Bubble/Bubble.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./Bubble.module.css";
 
-const Bubble = ({ children }) => {
-  return <div className={styles.bubble}>{children}</div>;
+const Bubble = ({ children, variant = "default" }) => {
+  return (
+    <div
+      className={`${styles.bubble} ${
+        variant === "pink" ? styles.bubblePink : ""
+      }`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Bubble;
