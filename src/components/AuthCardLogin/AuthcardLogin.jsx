47b81a98ba767e97fcdc8e70bb213002ee994e21
kenant68 <tekbask8@gmail.com>
2025-11-29 13:45:56 +0100

    bouton qui redirige vers la dashboard

--- src/components/AuthCardLogin/AuthcardLogin.jsx
+++ src/components/AuthCardLogin/AuthcardLogin.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
+import React from "react";
 import Button from "../button/Button.jsx";
 import styles from "./AuthcardLogin.module.css";
 import Input from "../Input/Input.jsx";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-
-const AuthcardLogin = ({title}) => {
-
-
-
-    return (
-        <div>
-            <section className={styles.card}>
-                <div className={styles.container}>
-                    <h1 className={styles.h1}>{title}</h1>
-                    <Input name={"Email"}></Input>
-                    <Input name={"Mot de passe"}></Input>
-                    <Button text={"Se connecter"}></Button>
-                    <div className={styles.linksRow}>
-                        <Link to="/register" className={styles.linkToRegister}>Créer un compte</Link>
-                        <Link to="/" className={styles.passwdForgotten}>Mot de passe oublié</Link>
-                    </div>
-                </div>
-            </section>
+const AuthcardLogin = ({ title }) => {
+  return (
+    <div>
+      <section className={styles.card}>
+        <div className={styles.container}>
+          <h1 className={styles.h1}>{title}</h1>
+          <Input name={"Email"}></Input>
+          <Input name={"Mot de passe"}></Input>
+          <Link to="/dashboard">
+            <Button text={"Se connecter"}></Button>
+          </Link>
+          <div className={styles.linksRow}>
+            <Link to="/register" className={styles.linkToRegister}>
+              Créer un compte
+            </Link>
+            <Link to="/" className={styles.passwdForgotten}>
+              Mot de passe oublié
+            </Link>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default AuthcardLogin;
--- src/components/AuthCardRegister/AuthcardRegister.jsx
+++ src/components/AuthCardRegister/AuthcardRegister.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
+import React from "react";
 import Button from "../button/Button.jsx";
 import Input from "../Input/Input.jsx";
 import styles from "./AuthcardRegister.module.css";
 import { Link } from "react-router-dom";
 
 const AuthcardRegister = ({ title }) => {
-    return (
-        <div>
-            <section className={styles.card}>
-                <div className={styles.container}>
-                    <h1 className={styles.h1}>{title}</h1>
-                    <Input name="Email" />
-                    <Input name="Mot de passe" />
-                    <Input name="Confirmez le mot de passe" />
-                    <Button className={styles["container-button"]} text="S'inscrire" />
+  return (
+    <div>
+      <section className={styles.card}>
+        <div className={styles.container}>
+          <h1 className={styles.h1}>{title}</h1>
+          <Input name="Email" />
+          <Input name="Mot de passe" />
+          <Input name="Confirmez le mot de passe" />
+          <Link to="/dashboard">
+            <Button className={styles["container-button"]} text="S'inscrire" />
+          </Link>
 
-
-                    <Link to="/login" className={styles.linkToLogin}>
-                        J'ai déjà un compte
-                    </Link>
-                </div>
-            </section>
+          <Link to="/login" className={styles.linkToLogin}>
+            J'ai déjà un compte
+          </Link>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default AuthcardRegister;
