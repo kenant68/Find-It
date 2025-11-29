863f906890adc603cb7bbf3255693f1cd13e2fea
kenant68 <tekbask8@gmail.com>
2025-11-29 13:26:56 +0100

    ajout de la page des equipes avec component pour rechercher et filtrer

---
+++ src/pages/other-teams/OtherTeams.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TeamSearch from "../../components/TeamSearch/TeamSearch.jsx";
+
+const OtherTeams = () => {
+  return <TeamSearch />;
+};
+
+export default OtherTeams;
